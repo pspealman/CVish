0a5d0a9820777e5a40fdff6600664b046e7fa016
Pieter Spealman <32845376+pspealman@users.noreply.github.com>
2021-10-30 20:38:35 -0400

    fixed typo

--- module_load.sh
+++ module_load.sh
@@ -1,5 +1,5 @@
 module load bwa/intel/0.7.17
 module load samtools/intel/1.14
 module load bedtools/intel/2.29.2
-module load velvet/intel/1.2.10
-module load blast+/intel/2.11.0
+module load velvet/1.2.10
+module load blast+/2.11.0
