9a2a228b97484ef15676608bcd5ccec21d60d53c
Pieter Spealman <32845376+pspealman@users.noreply.github.com>
2021-10-30 20:29:27 -0400

    Updated for compatibility to NYU HPC Greene

--- module_load.sh
+++ module_load.sh
@@ -1,7 +1,5 @@
-module load bwa/intel/0.7.15
-module load samtools/intel/1.6
-module load samblaster/intel/0.1.24
-module load numpy/intel/1.13.1
-module load bedtools/intel/2.26.0
+module load bwa/intel/0.7.17
+module load samtools/intel/1.14
+module load bedtools/intel/2.29.2
 module load velvet/intel/1.2.10
-module load blast+/intel/2.6.0
+module load blast+/intel/2.11.0
