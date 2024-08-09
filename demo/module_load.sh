c97091d63afb972b31d07185a0ff496a6673357b
Pieter Spealman <32845376+pspealman@users.noreply.github.com>
2024-08-09 14:43:35 -0400

    Update module_load.sh
    removed "module load velvet/1.2.10"

--- demo/module_load.sh
+++ demo/module_load.sh
@@ -1,7 +1,6 @@
 module load bwa/intel/0.7.17
 module load samtools/intel/1.14
 module load bedtools/intel/2.29.2
-module load velvet/1.2.10
 module load blast+/2.11.0
 module load samblaster/0.1.26
 module load mafft/intel/7.475
